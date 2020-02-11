348309f07ecf35e813b0c3c8ade794c08bd1fcc0
RSBlek <artur.blek@web.de>
2020-02-11 21:02:45 +0100

    Typo fix

--- Corvida.API.Canny.Demo/Program.cs
+++ Corvida.API.Canny.Demo/Program.cs
@@ -11,10 +11,10 @@ namespace Corvida.API.Canny.Demo
             CannyClient client = new CannyClient(apiKey);
             try
             {
-                using (Image watermarkedImage = client.CannyRequest(Image.FromFile("./img/demo.jpg"), 250, 150).Result)
+                using (Image cannyImage = client.CannyRequest(Image.FromFile("./img/demo.jpg"), 250, 150).Result)
                 {
                     String path = System.IO.Directory.GetCurrentDirectory() + "/img/canny.jpg";
-                    watermarkedImage.Save(path);
+                    cannyImage.Save(path);
                     Console.WriteLine("Canny image: " + path);
                     Console.ReadLine();
                 }
